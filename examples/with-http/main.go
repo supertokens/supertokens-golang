5ed79efedfa86ac78c3de944d0c88da16e981e2b
Rishabh <rishabh@supertokens.io>
2021-09-12 16:59:32 +0530

    adds with-http example

--- examples/with-http/main.go
+++ examples/with-http/main.go
@@ -1,5 +1,89 @@
 package main
 
+import (
+	"encoding/json"
+	"net/http"
+	"strings"
+
+	"github.com/supertokens/supertokens-golang/recipe/emailpassword"
+	"github.com/supertokens/supertokens-golang/recipe/session"
+	"github.com/supertokens/supertokens-golang/supertokens"
+)
+
 func main() {
-	// TODO:
+	err := supertokens.Init(supertokens.TypeInput{
+		Supertokens: &supertokens.SupertokenTypeInput{
+			ConnectionURI: "https://try.supertokens.io",
+		},
+		AppInfo: supertokens.AppInfo{
+			AppName:       "SuperTokens Demo App",
+			APIDomain:     "http://localhost:3001",
+			WebsiteDomain: "http://localhost:3000",
+		},
+		RecipeList: []supertokens.RecipeListFunction{
+			emailpassword.Init(nil),
+			session.Init(nil),
+		},
+	})
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	http.ListenAndServe(":3001", corsMiddleware(
+		supertokens.Middleware(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+			// Handle your APIs..
+
+			if r.URL.Path == "/sessioninfo" {
+				session.VerifySession(nil, sessioninfo).ServeHTTP(rw, r)
+				return
+			}
+		}))))
+}
+
+func corsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(response http.ResponseWriter, r *http.Request) {
+		response.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		response.Header().Set("Access-Control-Allow-Credentials", "true")
+		if r.Method == "OPTIONS" {
+			response.Header().Set("Access-Control-Allow-Headers", strings.Join(append([]string{"Content-Type"}, supertokens.GetAllCORSHeaders()...), ","))
+			response.Header().Set("Access-Control-Allow-Methods", "*")
+			response.Write([]byte(""))
+		} else {
+			next.ServeHTTP(response, r)
+		}
+	})
+}
+
+func sessioninfo(w http.ResponseWriter, r *http.Request) {
+	sessionContainer := session.GetSessionFromRequestContext(r.Context())
+
+	if sessionContainer == nil {
+		w.WriteHeader(500)
+		w.Write([]byte("no session found"))
+		return
+	}
+	sessionData, err := sessionContainer.GetSessionData()
+	if err != nil {
+		err = supertokens.ErrorHandler(err, r, w)
+		if err != nil {
+			w.WriteHeader(500)
+			w.Write([]byte(err.Error()))
+		}
+		return
+	}
+	w.WriteHeader(200)
+	w.Header().Add("content-type", "application/json")
+	bytes, err := json.Marshal(map[string]interface{}{
+		"sessionHandle": sessionContainer.GetHandle(),
+		"userId":        sessionContainer.GetUserID(),
+		"jwtPayload":    sessionContainer.GetJWTPayload(),
+		"sessionData":   sessionData,
+	})
+	if err != nil {
+		w.WriteHeader(500)
+		w.Write([]byte("error in converting to json"))
+	} else {
+		w.Write(bytes)
+	}
 }
