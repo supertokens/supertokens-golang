813c90f72547c89c36823b2390adc8eb09b85656
Rishabh <rishabh@supertokens.io>
2021-09-27 11:09:40 +0530

    adds api for jwt recipe

---
+++ recipe/jwt/api/getJWKS.go
@@ -0,0 +1,37 @@
+/* Copyright (c) 2021, VRAI Labs and/or its affiliates. All rights reserved.
+ *
+ * This software is licensed under the Apache License, Version 2.0 (the
+ * "License") as published by the Apache Software Foundation.
+ *
+ * You may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package api
+
+import (
+	"github.com/supertokens/supertokens-golang/recipe/jwt/jwtmodels"
+	"github.com/supertokens/supertokens-golang/supertokens"
+)
+
+func GetJWKS(apiImplementation jwtmodels.APIInterface, options jwtmodels.APIOptions) error {
+	if apiImplementation.GetJWKSGET == nil {
+		options.OtherHandler(options.Res, options.Req)
+		return nil
+	}
+
+	response, err := apiImplementation.GetJWKSGET(options)
+	if err != nil {
+		return err
+	}
+
+	return supertokens.Send200Response(options.Res, map[string]interface{}{
+		"keys": response.OK.Keys,
+	})
+}
