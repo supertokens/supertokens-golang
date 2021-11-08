9b32cb355e7a2f07b0eb5c2e292dad4ddb5921d2
Rishabh <rishabh@supertokens.io>
2021-11-08 13:39:58 +0530

    updates other examples as well

--- examples/with-http/main.go
+++ examples/with-http/main.go
@@ -25,19 +25,58 @@ func main() {
 		},
 		RecipeList: []supertokens.Recipe{
 			thirdpartyemailpassword.Init(&tpepmodels.TypeInput{
+				/*
+				   We use different credentials for different platforms when required. For example the redirect URI for Github
+				   is different for Web and mobile. In such a case we can provide multiple providers with different client Ids.
+
+				   When the frontend makes a request and wants to use a specific clientId, it needs to send the clientId to use in the
+				   request. In the absence of a clientId in the request the SDK uses the default provider, indicated by `isDefault: true`.
+				   When adding multiple providers for the same type (Google, Github etc), make sure to set `isDefault: true`.
+				*/
 				Providers: []tpmodels.TypeProvider{
 					// We have provided you with development keys which you can use for testsing.
 					// IMPORTANT: Please replace them with your own OAuth keys for production use.
 					thirdparty.Google(tpmodels.GoogleConfig{
+						// We use this for websites
+						IsDefault:    true,
 						ClientID:     "1060725074195-kmeum4crr01uirfl2op9kd5acmi9jutn.apps.googleusercontent.com",
 						ClientSecret: "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",
 					}),
+					thirdparty.Google(tpmodels.GoogleConfig{
+						// we use this for mobile apps
+						ClientID:     "1060725074195-c7mgk8p0h27c4428prfuo3lg7ould5o7.apps.googleusercontent.com",
+						ClientSecret: "", // this is empty because we follow Authorization code grant flow via PKCE for mobile apps (Google doesn't issue a client secret for mobile apps).
+					}),
 					thirdparty.Github(tpmodels.GithubConfig{
+						// We use this for websites
+						IsDefault:    true,
 						ClientID:     "467101b197249757c71f",
 						ClientSecret: "e97051221f4b6426e8fe8d51486396703012f5bd",
 					}),
+					thirdparty.Github(tpmodels.GithubConfig{
+						// We use this for mobile apps
+						ClientID:     "8a9152860ce869b64c44",
+						ClientSecret: "00e841f10f288363cd3786b1b1f538f05cfdbda2",
+					}),
+					/*
+					   For Apple signin, iOS apps always use the bundle identifier as the client ID when communicating with Apple. Android, Web and other platforms
+					   need to configure a Service ID on the Apple developer dashboard and use that as client ID.
+					   In the example below 4398792-io.supertokens.example.service is the client ID for Web. Android etc and thus we mark it as default. For iOS
+					   the frontend for the demo app sends the clientId in the request which is then used by the SDK.
+					*/
 					thirdparty.Apple(tpmodels.AppleConfig{
-						ClientID: "4398792-io.supertokens.example.service",
+						// For Android and website apps
+						IsDefault: true,
+						ClientID:  "4398792-io.supertokens.example.service",
+						ClientSecret: tpmodels.AppleClientSecret{
+							KeyId:      "7M48Y4RYDL",
+							PrivateKey: "-----BEGIN PRIVATE KEY-----\nMIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQgu8gXs+XYkqXD6Ala9Sf/iJXzhbwcoG5dMh1OonpdJUmgCgYIKoZIzj0DAQehRANCAASfrvlFbFCYqn3I2zeknYXLwtH30JuOKestDbSfZYxZNMqhF/OzdZFTV0zc5u5s3eN+oCWbnvl0hM+9IW0UlkdA\n-----END PRIVATE KEY-----",
+							TeamId:     "YWQCXGJRJL",
+						},
+					}),
+					thirdparty.Apple(tpmodels.AppleConfig{
+						// For iOS Apps
+						ClientID: "4398792-io.supertokens.example",
 						ClientSecret: tpmodels.AppleClientSecret{
 							KeyId:      "7M48Y4RYDL",
 							PrivateKey: "-----BEGIN PRIVATE KEY-----\nMIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQgu8gXs+XYkqXD6Ala9Sf/iJXzhbwcoG5dMh1OonpdJUmgCgYIKoZIzj0DAQehRANCAASfrvlFbFCYqn3I2zeknYXLwtH30JuOKestDbSfZYxZNMqhF/OzdZFTV0zc5u5s3eN+oCWbnvl0hM+9IW0UlkdA\n-----END PRIVATE KEY-----",
