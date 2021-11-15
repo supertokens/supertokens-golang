b132d98f8aebed4220e717f222c5228599923077
Rishabh <rishabh@supertokens.io>
2021-11-15 17:13:32 +0530

    more changes

--- recipe/session/middleware.go
+++ recipe/session/middleware.go
@@ -25,26 +25,27 @@ import (
 
 func VerifySessionHelper(recipeInstance Recipe, options *sessmodels.VerifySessionOptions, otherHandler http.HandlerFunc) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		dw := &supertokens.DoneWriter{ResponseWriter: w}
 		session, err := (*recipeInstance.APIImpl.VerifySession)(options, sessmodels.APIOptions{
 			Config:               recipeInstance.Config,
 			OtherHandler:         otherHandler,
 			Req:                  r,
-			Res:                  w,
+			Res:                  dw,
 			RecipeID:             recipeInstance.RecipeModule.GetRecipeID(),
 			RecipeImplementation: recipeInstance.RecipeImpl,
 		})
 		if err != nil {
-			err = supertokens.ErrorHandler(err, r, w)
+			err = supertokens.ErrorHandler(err, r, dw)
 			if err != nil {
-				recipeInstance.RecipeModule.OnGeneralError(err, r, w)
+				recipeInstance.RecipeModule.OnGeneralError(err, r, dw)
 			}
 			return
 		}
 		if session != nil {
 			ctx := context.WithValue(r.Context(), sessmodels.SessionContext, session)
-			otherHandler(w, r.WithContext(ctx))
+			otherHandler(dw, r.WithContext(ctx))
 		} else {
-			otherHandler(w, r)
+			otherHandler(dw, r)
 		}
 	})
 }
