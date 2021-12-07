7eebf317e8a2f0c733eefeb0e3226a800c177b02
Rishabh <rishabh@supertokens.io>
2021-12-07 21:26:17 +0530

    adds missing copy right

--- recipe/openid/constants.go
+++ recipe/openid/constants.go
@@ -13,19 +13,8 @@
  * under the License.
  */
 
-package openidmodels
+package openid
 
-import "net/http"
-
-type APIOptions struct {
-	RecipeImplementation RecipeInterface
-	Config               TypeNormalisedInput
-	RecipeID             string
-	Req                  *http.Request
-	Res                  http.ResponseWriter
-	OtherHandler         http.HandlerFunc
-}
-
-type APIInterface struct {
-	GetOpenIdDiscoveryConfigurationGET *func(options APIOptions) (GetOpenIdDiscoveryConfigurationResponse, error)
-}
+const (
+	GetDiscoveryConfigUrl = "/.well-known/openid-configuration"
+)
