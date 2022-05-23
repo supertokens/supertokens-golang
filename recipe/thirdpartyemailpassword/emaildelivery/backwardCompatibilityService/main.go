d51ffde05cf0b3d460310645ba7ec9aaefc827f5
Sattvik Chakravarthy <sattvik@gmail.com>
2022-05-23 16:05:49 +0530

    fix: PR comments

--- recipe/thirdpartyemailpassword/emaildelivery/backwardCompatibilityService/main.go
+++ recipe/thirdpartyemailpassword/emaildelivery/backwardCompatibilityService/main.go
@@ -6,7 +6,6 @@ import (
 	"github.com/supertokens/supertokens-golang/ingredients/emaildelivery"
 	emailPasswordBackwardsCompatibilityService "github.com/supertokens/supertokens-golang/recipe/emailpassword/emaildelivery/backwardCompatibilityService"
 	"github.com/supertokens/supertokens-golang/recipe/emailpassword/epmodels"
-	emailVerificationBackwardsCompatibilityService "github.com/supertokens/supertokens-golang/recipe/emailverification/emaildelivery/backwardCompatibilityService"
 	"github.com/supertokens/supertokens-golang/recipe/emailverification/evmodels"
 	"github.com/supertokens/supertokens-golang/recipe/thirdpartyemailpassword/tpepmodels"
 	"github.com/supertokens/supertokens-golang/supertokens"
@@ -15,14 +14,10 @@ import (
 func MakeBackwardCompatibilityService(recipeInterfaceImpl tpepmodels.RecipeInterface, emailPasswordRecipeInterfaceImpl epmodels.RecipeInterface, appInfo supertokens.NormalisedAppinfo, sendEmailVerificationEmail func(user evmodels.User, emailVerificationURLWithToken string, userContext supertokens.UserContext), sendResetPasswordEmail func(user epmodels.User, passwordResetURLWithToken string, userContext supertokens.UserContext)) emaildelivery.EmailDeliveryInterface {
 	// We are using evmodels.User as opposed to tpmodels.User because TypeInput of thirdparty accepts evmodels.TypeInput for EmailVerificationFeature
 	// Similarly with epmodels.User as well
-	emailVerificationService := emailVerificationBackwardsCompatibilityService.MakeBackwardCompatibilityService(appInfo, sendEmailVerificationEmail)
 	emailPasswordService := emailPasswordBackwardsCompatibilityService.MakeBackwardCompatibilityService(emailPasswordRecipeInterfaceImpl, appInfo, sendResetPasswordEmail, sendEmailVerificationEmail)
 
 	sendEmail := func(input emaildelivery.EmailType, userContext supertokens.UserContext) error {
-		if input.EmailVerification != nil {
-			return (*emailVerificationService.SendEmail)(input, userContext)
-
-		} else if input.PasswordReset != nil {
+		if input.EmailVerification != nil || input.PasswordReset != nil {
 			return (*emailPasswordService.SendEmail)(input, userContext)
 
 		} else {
