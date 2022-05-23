585035b731fa92bf5092fffb9d26f5c08b74d1d3
Sattvik Chakravarthy <sattvik@gmail.com>
2022-05-23 16:39:39 +0530

    refactor: removed unnecessary wrapper

--- recipe/thirdpartypasswordless/emaildelivery/backwardCompatibilityService/main.go
+++ recipe/thirdpartypasswordless/emaildelivery/backwardCompatibilityService/main.go
@@ -12,19 +12,6 @@ import (
 )
 
 func MakeBackwardCompatibilityService(recipeInterfaceImpl tplmodels.RecipeInterface, appInfo supertokens.NormalisedAppinfo, sendEmailVerificationEmail func(user evmodels.User, emailVerificationURLWithToken string, userContext supertokens.UserContext), sendPasswordlessLoginEmail func(email string, userInputCode *string, urlWithLinkCode *string, codeLifetime uint64, preAuthSessionId string, userContext supertokens.UserContext) error) emaildelivery.EmailDeliveryInterface {
-	if sendEmailVerificationEmail != nil {
-		inputSendEmailVerificationEmail := sendEmailVerificationEmail
-		sendEmailVerificationEmail = func(user evmodels.User, emailVerificationURLWithToken string, userContext supertokens.UserContext) {
-			userInfo, err := (*recipeInterfaceImpl.GetUserByID)(user.ID, userContext)
-			if err != nil {
-				return // FIXME: No error handling here
-			}
-			if userInfo.Email == nil {
-				return // FIXME: No error handling here
-			}
-			inputSendEmailVerificationEmail(evmodels.User{ID: userInfo.ID, Email: *userInfo.Email}, emailVerificationURLWithToken, userContext)
-		}
-	}
 	emailVerificationService := emailVerificationBackwardsCompatibilityService.MakeBackwardCompatibilityService(appInfo, sendEmailVerificationEmail)
 	passwordlessService := passwordlessBackwardsCompatibilityService.MakeBackwardCompatibilityService(appInfo, sendPasswordlessLoginEmail)
 
