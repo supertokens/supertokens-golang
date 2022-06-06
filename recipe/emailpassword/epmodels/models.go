94bb1f1d584bd50711134813feb437f4935a1b29
Sattvik Chakravarthy <sattvik@gmail.com>
2022-06-06 17:36:02 +0530

    refactor: adding missed deprecation

--- recipe/emailpassword/epmodels/models.go
+++ recipe/emailpassword/epmodels/models.go
@@ -38,7 +38,7 @@ type OverrideStruct struct {
 
 type TypeInputEmailVerificationFeature struct {
 	GetEmailVerificationURL  func(user User, userContext supertokens.UserContext) (string, error)
-	CreateAndSendCustomEmail func(user User, emailVerificationURLWithToken string, userContext supertokens.UserContext)
+	CreateAndSendCustomEmail func(user User, emailVerificationURLWithToken string, userContext supertokens.UserContext) // Deprecated: Use EmailDelivery instead.
 }
 
 type TypeInputFormField struct {
