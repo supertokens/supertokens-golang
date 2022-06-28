fe658b4884bbbde73d2ecb55b9f03862554bb9d2
Sattvik Chakravarthy <sattvik@gmail.com>
2022-06-28 13:30:08 +0530

    feat: user roles recipe impl

---
+++ recipe/userroles/userrolesmodels/recipeInterface.go
@@ -0,0 +1,97 @@
+/* Copyright (c) 2021, VRAI Labs and/or its affiliates. All rights reserved.
+ *
+ * This software is licensed under the Apache License, Version 2.0 (the
+ * "License") as published by the Apache Software Foundation.
+ *
+ * You may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package userrolesmodels
+
+import "github.com/supertokens/supertokens-golang/supertokens"
+
+type UnknownRoleError struct {
+}
+
+type AddRoleToUserResponse struct {
+	OK *struct {
+		DidUserAlreadyHaveRole bool
+	}
+	UnknownRoleError *UnknownRoleError
+}
+
+type RemoveUserRoleResponse struct {
+	OK *struct {
+		DidUserHaveRole bool
+	}
+	UnknownRoleError *UnknownRoleError
+}
+
+type GetRolesForUserResponse struct {
+	OK *struct {
+		Roles []string
+	}
+}
+
+type GetUsersThatHaveRoleResponse struct {
+	OK *struct {
+		Users []string
+	}
+	UnknownRoleError *UnknownRoleError
+}
+
+type CreateNewRoleOrAddPermissionsResponse struct {
+	OK *struct {
+		CreatedNewRole bool
+	}
+}
+
+type GetPermissionsForRoleResponse struct {
+	OK *struct {
+		Permissions []string
+	}
+	UnknownRoleError *UnknownRoleError
+}
+
+type RemovePermissionsFromRoleResponse struct {
+	OK               *struct{}
+	UnknownRoleError *UnknownRoleError
+}
+
+type GetRolesThatHavePermissionResponse struct {
+	OK *struct {
+		Roles []string
+	}
+}
+
+type DeleteRoleResponse struct {
+	OK *struct {
+		DidRoleExist bool
+	}
+}
+
+type GetAllRolesResponse struct {
+	OK *struct {
+		Roles []string
+	}
+}
+
+type RecipeInterface struct {
+	AddRoleToUser                 *func(userID string, role string, userContext supertokens.UserContext) (AddRoleToUserResponse, error)
+	RemoveUserRole                *func(userID string, role string, userContext supertokens.UserContext) (RemoveUserRoleResponse, error)
+	GetRolesForUser               *func(userID string, userContext supertokens.UserContext) (GetRolesForUserResponse, error)
+	GetUsersThatHaveRole          *func(role string, userContext supertokens.UserContext) (GetUsersThatHaveRoleResponse, error)
+	CreateNewRoleOrAddPermissions *func(role string, permissions []string, userContext supertokens.UserContext) (CreateNewRoleOrAddPermissionsResponse, error)
+	GetPermissionsForRole         *func(role string, userContext supertokens.UserContext) (GetPermissionsForRoleResponse, error)
+	RemovePermissionsFromRole     *func(role string, permissions []string, userContext supertokens.UserContext) (RemovePermissionsFromRoleResponse, error)
+	GetRolesThatHavePermission    *func(permission string, userContext supertokens.UserContext) (GetRolesThatHavePermissionResponse, error)
+	DeleteRole                    *func(role string, userContext supertokens.UserContext) (DeleteRoleResponse, error)
+	GetAllRoles                   *func(userContext supertokens.UserContext) (GetAllRolesResponse, error)
+}
---
+++ recipe/userroles/utils.go
@@ -0,0 +1,50 @@
+/* Copyright (c) 2022, VRAI Labs and/or its affiliates. All rights reserved.
+ *
+ * This software is licensed under the Apache License, Version 2.0 (the
+ * "License") as published by the Apache Software Foundation.
+ *
+ * You may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package userroles
+
+import (
+	"github.com/supertokens/supertokens-golang/recipe/userroles/userrolesmodels"
+	"github.com/supertokens/supertokens-golang/supertokens"
+)
+
+func validateAndNormaliseUserInput(appInfo supertokens.NormalisedAppinfo, config *userrolesmodels.TypeInput) userrolesmodels.TypeNormalisedInput {
+
+	typeNormalisedInput := makeTypeNormalisedInput(appInfo)
+
+	if config != nil && config.Override != nil {
+		if config.Override.Functions != nil {
+			typeNormalisedInput.Override.Functions = config.Override.Functions
+		}
+		if config.Override.APIs != nil {
+			typeNormalisedInput.Override.APIs = config.Override.APIs
+		}
+	}
+
+	return typeNormalisedInput
+}
+
+func makeTypeNormalisedInput(appInfo supertokens.NormalisedAppinfo) userrolesmodels.TypeNormalisedInput {
+	return userrolesmodels.TypeNormalisedInput{
+		Override: userrolesmodels.OverrideStruct{
+			Functions: func(originalImplementation userrolesmodels.RecipeInterface) userrolesmodels.RecipeInterface {
+				return originalImplementation
+			},
+			APIs: func(originalImplementation userrolesmodels.APIInterface) userrolesmodels.APIInterface {
+				return originalImplementation
+			},
+		},
+	}
+}
