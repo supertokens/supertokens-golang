4f614343cf4126535cd6cc9984859643d3235cc3
Sattvik Chakravarthy <sattvik@gmail.com>
2022-09-01 13:12:54 +0530

    fix: claim to pointers

--- recipe/userroles/userrolesclaims/claims.go
+++ recipe/userroles/userrolesclaims/claims.go
@@ -6,12 +6,12 @@ type TypeUserRoleClaimValidators struct {
 	claims.PrimitiveArrayClaimValidators
 }
 
-var UserRoleClaim claims.TypeSessionClaim
+var UserRoleClaim *claims.TypeSessionClaim
 var UserRoleClaimValidators TypeUserRoleClaimValidators
 
 type TypePermissionClaimValidators struct {
 	claims.PrimitiveArrayClaimValidators
 }
 
-var PermissionClaim claims.TypeSessionClaim
+var PermissionClaim *claims.TypeSessionClaim
 var PermissionClaimValidators TypePermissionClaimValidators
