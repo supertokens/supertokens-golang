ef41b966b8f6737b3e67ed6cd9cb9772cc122d2a
Sattvik Chakravarthy <sattvik@gmail.com>
2022-11-08 10:03:17 +0530

    fix: providersForTenantGET API interface

--- recipe/thirdparty/tpmodels/apiInterface.go
+++ recipe/thirdparty/tpmodels/apiInterface.go
@@ -24,7 +24,7 @@ import (
 )
 
 type APIInterface struct {
-	ProvidersForTenantGET    *func(tenantId string) (ProvidersForTenantGetResponse, error)
+	ProvidersForTenantGET    *func(tenantId string, userContext supertokens.UserContext) (ProvidersForTenantGetResponse, error)
 	AuthorisationUrlGET      *func(provider TypeProvider, clientType *string, tenantId *string, redirectURIOnProviderDashboard string, options APIOptions, userContext supertokens.UserContext) (AuthorisationUrlGETResponse, error)
 	SignInUpPOST             *func(provider TypeProvider, clientType *string, tenantId *string, input TypeSignInUpInput, options APIOptions, userContext supertokens.UserContext) (SignInUpPOSTResponse, error)
 	AppleRedirectHandlerPOST *func(formPostInfoFromProvider map[string]interface{}, options APIOptions, userContext supertokens.UserContext) error
