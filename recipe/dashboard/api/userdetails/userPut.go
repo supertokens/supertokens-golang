ca7d1ce7345cc522d02648b8b6b15606218cee0e
Nemi Shah <nemishah1212@gmail.com>
2022-11-23 10:46:06 +0530

    Remove logs

--- recipe/dashboard/api/userdetails/userPut.go
+++ recipe/dashboard/api/userdetails/userPut.go
@@ -18,7 +18,6 @@ package userdetails
 import (
 	"encoding/json"
 	"errors"
-	"fmt"
 	"strings"
 
 	"github.com/supertokens/supertokens-golang/recipe/dashboard/api"
@@ -386,7 +385,6 @@ func UserPut(apiInterface dashboardmodels.APIInterface, options dashboardmodels.
 	_, recipeId := api.GetUserForRecipeId(readBody.UserId, readBody.RecipeId)
 
 	if readBody.FirstName != "" || readBody.LastName != "" {
-		fmt.Println("First name or last name present")
 		isRecipeInitialised := false
 
 		_, err = usermetadata.GetRecipeInstanceOrThrowError()
@@ -394,8 +392,6 @@ func UserPut(apiInterface dashboardmodels.APIInterface, options dashboardmodels.
 		if err == nil {
 			isRecipeInitialised = true
 		}
-
-		fmt.Println(isRecipeInitialised)
 
 		if isRecipeInitialised {
 			metadataupdate := make(map[string]interface{})
@@ -407,8 +403,6 @@ func UserPut(apiInterface dashboardmodels.APIInterface, options dashboardmodels.
 			if strings.TrimSpace(readBody.LastName) != "" {
 				metadataupdate["last_name"] = strings.TrimSpace(readBody.LastName)
 			}
-
-			fmt.Println(metadataupdate)
 
 			usermetadata.UpdateUserMetadata(readBody.UserId, metadataupdate)
 		}
