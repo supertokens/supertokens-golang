a5d663397ec380e6c462b4b39f7b782ffc89da0b
Sattvik Chakravarthy <sattvik@gmail.com>
2022-12-14 10:11:53 +0530

    fix: removed idrefreshtoken

--- recipe/session/cookieAndHeaders.go
+++ recipe/session/cookieAndHeaders.go
@@ -36,10 +36,6 @@ const (
 	refreshTokenCookieKey  = "sRefreshToken"
 	refreshTokenHeaderKey  = "st-refresh-token"
 
-	// there are two of them because one is used by the server to check if the user is logged in and the other is checked by the frontend to see if the user is logged in.
-	idRefreshTokenCookieKey = "sIdRefreshToken"
-	idRefreshTokenHeaderKey = "id-refresh-token"
-
 	antiCsrfHeaderKey = "anti-csrf"
 	ridHeaderKey      = "rid"
 
