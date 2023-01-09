940cc8ecae7bb133128aed140ad659f7463f4d4f
Sattvik Chakravarthy <sattvik@gmail.com>
2023-01-09 12:49:22 +0530

    fix: email formatting

--- recipe/thirdparty/providers/custom.go
+++ recipe/thirdparty/providers/custom.go
@@ -17,6 +17,7 @@ package providers
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/supertokens/supertokens-golang/recipe/thirdparty/tperrors"
 	"github.com/supertokens/supertokens-golang/recipe/thirdparty/tpmodels"
@@ -40,6 +41,7 @@ func NewProvider(input tpmodels.ProviderInput) *tpmodels.TypeProvider {
 
 	if input.Config.GenerateFakeEmail == nil {
 		input.Config.GenerateFakeEmail = func(thirdPartyUserId string, userContext supertokens.UserContext) string {
+			thirdPartyUserId = strings.ReplaceAll(thirdPartyUserId, "|", ".tenant-")
 			return fmt.Sprintf("%s@%s.fakeemail.com", thirdPartyUserId, input.Config.ThirdPartyId)
 		}
 	}
