a2a3447051f22b265560286aafa619b088386bad
Iresh Sharma <32684272+iresharma@users.noreply.github.com>
2023-04-28 16:34:12 +0530

    Update constants.go

--- supertokens/constants.go
+++ supertokens/constants.go
@@ -21,7 +21,7 @@ const (
 )
 
 // VERSION current version of the lib
-const VERSION = "0.11.8"
+const VERSION = "0.11.0"
 
 var (
 	cdiSupported = []string{"2.8", "2.9", "2.10", "2.11", "2.12", "2.13", "2.14", "2.15", "2.16", "2.17", "2.18", "2.19", "2.20"}
