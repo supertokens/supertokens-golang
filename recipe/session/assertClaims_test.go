ff5b810138a847ffeaa7052bf2c293e2bf163f3e
Nemi Shah <nemishah1212@gmail.com>
2023-05-04 16:00:29 +0530

    Update session tests

--- recipe/session/assertClaims_test.go
+++ recipe/session/assertClaims_test.go
@@ -61,71 +61,86 @@ func TestEmptyClaimsArray(t *testing.T) {
 	assert.NoError(t, err)
 }
 
-// TODO NEMI: Commenting because payload structure has changed, will fix later
-//func TestAssertClaimsWithPayload(t *testing.T) {
-//	configValue := supertokens.TypeInput{
-//		Supertokens: &supertokens.ConnectionInfo{
-//			ConnectionURI: "http://localhost:8080",
-//		},
-//		AppInfo: supertokens.AppInfo{
-//			AppName:       "SuperTokens",
-//			WebsiteDomain: "supertokens.io",
-//			APIDomain:     "api.supertokens.io",
-//		},
-//		RecipeList: []supertokens.Recipe{
-//			Init(&sessmodels.TypeInput{
-//				GetTokenTransferMethod: func(req *http.Request, forCreateNewSession bool, userContext supertokens.UserContext) sessmodels.TokenTransferMethod {
-//					return sessmodels.CookieTransferMethod
-//				},
-//			}),
-//		},
-//	}
-//	BeforeEach()
-//	unittesting.StartUpST("localhost", "8080")
-//	defer AfterEach()
-//	err := supertokens.Init(configValue)
-//	if err != nil {
-//		t.Error(err.Error())
-//	}
-//
-//	mux := http.NewServeMux()
-//	var sessionContainer sessmodels.SessionContainer
-//	accessTokenPayload := map[string]interface{}{
-//		"hello": "world",
-//	}
-//
-//	mux.HandleFunc("/create", func(rw http.ResponseWriter, r *http.Request) {
-//		var err error
-//		sessionContainer, err = CreateNewSession(r, rw, "rope", accessTokenPayload, map[string]interface{}{})
-//		assert.NoError(t, err)
-//	})
-//
-//	testServer := httptest.NewServer(supertokens.Middleware(mux))
-//	defer func() {
-//		testServer.Close()
-//	}()
-//	req, err := http.NewRequest(http.MethodGet, testServer.URL+"/create", nil)
-//	assert.NoError(t, err)
-//	res, err := http.DefaultClient.Do(req)
-//	assert.NoError(t, err)
-//	assert.Equal(t, 200, res.StatusCode)
-//
-//	validateCallCount := 0
-//	var validationPayload map[string]interface{}
-//
-//	validate := func(payload map[string]interface{}, userContext supertokens.UserContext) claims.ClaimValidationResult {
-//		validateCallCount += 1
-//		validationPayload = payload
-//		return claims.ClaimValidationResult{
-//			IsValid: true,
-//		}
-//	}
-//
-//	_, validators := StubClaim(validate)
-//	err = sessionContainer.AssertClaims([]claims.SessionClaimValidator{
-//		validators.Stub(),
-//	})
-//	assert.NoError(t, err)
-//	assert.Equal(t, 1, validateCallCount)
-//	assert.Equal(t, accessTokenPayload, validationPayload)
-//}
+func TestAssertClaimsWithPayload(t *testing.T) {
+	configValue := supertokens.TypeInput{
+		Supertokens: &supertokens.ConnectionInfo{
+			ConnectionURI: "http://localhost:8080",
+		},
+		AppInfo: supertokens.AppInfo{
+			AppName:       "SuperTokens",
+			WebsiteDomain: "supertokens.io",
+			APIDomain:     "api.supertokens.io",
+		},
+		RecipeList: []supertokens.Recipe{
+			Init(&sessmodels.TypeInput{
+				GetTokenTransferMethod: func(req *http.Request, forCreateNewSession bool, userContext supertokens.UserContext) sessmodels.TokenTransferMethod {
+					return sessmodels.CookieTransferMethod
+				},
+			}),
+		},
+	}
+	BeforeEach()
+	unittesting.StartUpST("localhost", "8080")
+	defer AfterEach()
+	err := supertokens.Init(configValue)
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	mux := http.NewServeMux()
+	var sessionContainer sessmodels.SessionContainer
+	accessTokenPayload := map[string]interface{}{
+		"hello": "world",
+	}
+
+	mux.HandleFunc("/create", func(rw http.ResponseWriter, r *http.Request) {
+		var err error
+		sessionContainer, err = CreateNewSession(r, rw, "rope", accessTokenPayload, map[string]interface{}{})
+		assert.NoError(t, err)
+	})
+
+	testServer := httptest.NewServer(supertokens.Middleware(mux))
+	defer func() {
+		testServer.Close()
+	}()
+	req, err := http.NewRequest(http.MethodGet, testServer.URL+"/create", nil)
+	assert.NoError(t, err)
+	res, err := http.DefaultClient.Do(req)
+	assert.NoError(t, err)
+	assert.Equal(t, 200, res.StatusCode)
+
+	validateCallCount := 0
+	var validationPayload map[string]interface{}
+
+	validate := func(payload map[string]interface{}, userContext supertokens.UserContext) claims.ClaimValidationResult {
+		validateCallCount += 1
+		validationPayload = payload
+		return claims.ClaimValidationResult{
+			IsValid: true,
+		}
+	}
+
+	_, validators := StubClaim(validate)
+	err = sessionContainer.AssertClaims([]claims.SessionClaimValidator{
+		validators.Stub(),
+	})
+	assert.NoError(t, err)
+	assert.Equal(t, 1, validateCallCount)
+	expectedKeys := []string{
+		"exp",
+		"iat",
+		"iss",
+		"parentRefreshTokenHash1",
+		"refreshTokenHash1",
+		"sessionHandle",
+		"sub",
+		"hello",
+	}
+
+	for _, k := range expectedKeys {
+		_, ok := validationPayload[k]
+		assert.True(t, ok)
+	}
+
+	assert.True(t, validationPayload["hello"] == "world")
+}
