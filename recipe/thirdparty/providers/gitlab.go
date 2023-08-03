e2d7ab40803cfd5dbfcc0bbc3b0d656e1c3fae60
Sattvik Chakravarthy <sattvik@supertokens.com>
2023-08-03 19:10:46 +0530

    fix: gitlab and bitbucket impl (#327)
    * fix: providers
    
    * fix: gitlab and bitbucket impl

--- recipe/thirdparty/providers/gitlab.go
+++ recipe/thirdparty/providers/gitlab.go
@@ -17,124 +17,48 @@ package providers
 
 import (
 	"github.com/supertokens/supertokens-golang/recipe/thirdparty/tpmodels"
+	"github.com/supertokens/supertokens-golang/supertokens"
 )
 
 const gitlabID = "gitlab"
 
-func GitLab(config tpmodels.ProviderInput) tpmodels.TypeProvider {
-	// FIXME
-	// gitLabURL := "https://gitlab.com"
-	// if config.GitLabBaseURL != nil {
-	// 	url, err := supertokens.NewNormalisedURLDomain(*config.GitLabBaseURL)
-	// 	if err != nil {
-	// 		panic(err)
-	// 	}
-	// 	gitLabURL = url.GetAsStringDangerous()
-	// }
-	return tpmodels.TypeProvider{
-		// ID: gitlabID,
-		// Get: func(redirectURI, authCodeFromRequest *string, userContext supertokens.UserContext) tpmodels.TypeProviderGetResponse {
-		// 	accessTokenAPIURL := gitLabURL + "/oauth/token"
-		// 	accessTokenAPIParams := map[string]string{
-		// 		"client_id":     config.ClientID,
-		// 		"client_secret": config.ClientSecret,
-		// 		"grant_type":    "authorization_code",
-		// 	}
-		// 	if authCodeFromRequest != nil {
-		// 		accessTokenAPIParams["code"] = *authCodeFromRequest
-		// 	}
-		// 	if redirectURI != nil {
-		// 		accessTokenAPIParams["redirect_uri"] = *redirectURI
-		// 	}
-
-		// 	authorisationRedirectURL := gitLabURL + "/oauth/authorize"
-		// 	scopes := []string{"read_user"}
-		// 	if config.Scope != nil {
-		// 		scopes = config.Scope
-		// 	}
-
-		// 	var additionalParams map[string]interface{} = nil
-		// 	if config.AuthorisationRedirect != nil && config.AuthorisationRedirect.Params != nil {
-		// 		additionalParams = config.AuthorisationRedirect.Params
-		// 	}
-
-		// 	authorizationRedirectParams := map[string]interface{}{
-		// 		"scope":         strings.Join(scopes, " "),
-		// 		"response_type": "code",
-		// 		"client_id":     config.ClientID,
-		// 	}
-		// 	for key, value := range additionalParams {
-		// 		authorizationRedirectParams[key] = value
-		// 	}
-
-		// 	return tpmodels.TypeProviderGetResponse{
-		// 		AccessTokenAPI: tpmodels.AccessTokenAPI{
-		// 			URL:    accessTokenAPIURL,
-		// 			Params: accessTokenAPIParams,
-		// 		},
-		// 		AuthorisationRedirect: tpmodels.AuthorisationRedirect{
-		// 			URL:    authorisationRedirectURL,
-		// 			Params: authorizationRedirectParams,
-		// 		},
-		// 		GetProfileInfo: func(authCodeResponse interface{}, userContext supertokens.UserContext) (tpmodels.UserInfo, error) {
-		// 			authCodeResponseJson, err := json.Marshal(authCodeResponse)
-		// 			if err != nil {
-		// 				return tpmodels.UserInfo{}, err
-		// 			}
-		// 			var accessTokenAPIResponse gitlabGetProfileInfoInput
-		// 			err = json.Unmarshal(authCodeResponseJson, &accessTokenAPIResponse)
-		// 			if err != nil {
-		// 				return tpmodels.UserInfo{}, err
-		// 			}
-		// 			accessToken := accessTokenAPIResponse.AccessToken
-		// 			authHeader := "Bearer " + accessToken
-		// 			response, err := getGitLabAuthRequest(gitLabURL, authHeader)
-		// 			if err != nil {
-		// 				return tpmodels.UserInfo{}, err
-		// 			}
-		// 			userInfo := response.(map[string]interface{})
-		// 			ID := fmt.Sprint(userInfo["id"]) // the id returned by gitlab is a number, so we convert to a string
-		// 			_, emailExists := userInfo["email"]
-		// 			if !emailExists {
-		// 				return tpmodels.UserInfo{
-		// 					ID: ID,
-		// 				}, nil
-		// 			}
-		// 			email := userInfo["email"].(string)
-		// 			var isVerified bool
-		// 			_, ok := userInfo["confirmed_at"]
-		// 			if ok && userInfo["confirmed_at"] != nil && userInfo["confirmed_at"].(string) != "" {
-		// 				isVerified = true
-		// 			} else {
-		// 				isVerified = false
-		// 			}
-		// 			return tpmodels.UserInfo{
-		// 				ID: ID,
-		// 				Email: &tpmodels.EmailStruct{
-		// 					ID:         email,
-		// 					IsVerified: isVerified,
-		// 				},
-		// 			}, nil
-		// 		},
-		// 		GetClientId: func(userContext supertokens.UserContext) string {
-		// 			return config.ClientID
-		// 		},
-		// 	}
-		// },
-		// IsDefault: config.IsDefault,
+func Gitlab(input tpmodels.ProviderInput) *tpmodels.TypeProvider {
+	if input.Config.Name == "" {
+		input.Config.Name = "Gitlab"
 	}
+
+	oOverride := input.Override
+
+	input.Override = func(originalImplementation *tpmodels.TypeProvider) *tpmodels.TypeProvider {
+		oGetConfig := originalImplementation.GetConfigForClientType
+		originalImplementation.GetConfigForClientType = func(clientType *string, userContext supertokens.UserContext) (tpmodels.ProviderConfigForClientType, error) {
+			config, err := oGetConfig(clientType, userContext)
+			if err != nil {
+				return tpmodels.ProviderConfigForClientType{}, err
+			}
+
+			if len(config.Scope) == 0 {
+				config.Scope = []string{"openid", "email"}
+			}
+
+			if config.OIDCDiscoveryEndpoint == "" {
+				config.OIDCDiscoveryEndpoint = "https://gitlab.com"
+				if config.AdditionalConfig != nil {
+					if gitlabBaseUrl, ok := config.AdditionalConfig["gitlabBaseUrl"].(string); ok {
+						config.OIDCDiscoveryEndpoint = gitlabBaseUrl
+					}
+				}
+			}
+
+			return config, nil
+		}
+
+		if oOverride != nil {
+			originalImplementation = oOverride(originalImplementation)
+		}
+
+		return originalImplementation
+	}
+
+	return NewProvider(input)
 }
-
-// func getGitLabAuthRequest(gitLabUrl string, authHeader string) (interface{}, error) {
-// 	url := gitLabUrl + "/api/v4/user"
-// 	req, err := http.NewRequest("GET", url, nil)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	req.Header.Add("Authorization", authHeader)
-// 	return doGetRequest(req)
-// }
-
-// type gitlabGetProfileInfoInput struct {
-// 	AccessToken string `json:"access_token"`
-// }
