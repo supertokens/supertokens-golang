91871182975595764247349779d6bae89796ff5b
rishabhpoddar <rishabh.poddar@gmail.com>
2023-12-12 17:26:06 +0530

    adds more tests

--- recipe/emailpassword/accountlinkingRecipeImplementation_test.go
+++ recipe/emailpassword/accountlinkingRecipeImplementation_test.go
@@ -131,3 +131,265 @@ func TestGetOldestUsersFirst(t *testing.T) {
 		}
 	}
 }
+
+func TestGetNewestUsersFirst(t *testing.T) {
+	BeforeEach()
+	unittesting.StartUpST("localhost", "8080")
+	defer AfterEach()
+	telemetry := false
+	supertokens.Init(supertokens.TypeInput{
+		Supertokens: &supertokens.ConnectionInfo{
+			ConnectionURI: "http://localhost:8080",
+		},
+		AppInfo: supertokens.AppInfo{
+			AppName:   "Testing",
+			Origin:    "http://localhost:3000",
+			APIDomain: "http://localhost:3001",
+		},
+		Telemetry: &telemetry,
+		RecipeList: []supertokens.Recipe{
+			Init(nil),
+		},
+	})
+
+	_, err := SignUp("public", "test@gmail.com", "testPass123")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	_, err = SignUp("public", "test1@gmail.com", "testPass123")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	_, err = SignUp("public", "test2@gmail.com", "testPass123")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	_, err = SignUp("public", "test3@gmail.com", "testPass123")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	user5, err := SignUp("public", "test4@gmail.com", "testPass123")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	{
+		paginationResult, err := supertokens.GetUsersNewestFirst("public", nil, nil, nil, nil)
+		if err != nil {
+			t.Error(err)
+			return
+		}
+
+		email := "test4@gmail.com"
+		assert.Nil(t, paginationResult.NextPaginationToken)
+		assert.Len(t, paginationResult.Users, 5)
+		assert.True(t, paginationResult.Users[0].LoginMethods[0].HasSameEmailAs(&email))
+		assert.Equal(t, paginationResult.Users[0].ID, user5.OK.User.ID)
+		assert.Equal(t, paginationResult.Users[0].ID, paginationResult.Users[0].LoginMethods[0].RecipeUserID.GetAsString())
+	}
+
+	{
+		limit := 1
+		paginationResult, err := supertokens.GetUsersNewestFirst("public", nil, &limit, nil, nil)
+		if err != nil {
+			t.Error(err)
+			return
+		}
+
+		assert.NotNil(t, paginationResult.NextPaginationToken)
+		assert.Len(t, paginationResult.Users, 1)
+		assert.Equal(t, paginationResult.Users[0].Emails[0], "test4@gmail.com")
+
+		paginationResult, err = supertokens.GetUsersNewestFirst("public", paginationResult.NextPaginationToken, &limit, nil, nil)
+		if err != nil {
+			t.Error(err)
+			return
+		}
+
+		assert.NotNil(t, paginationResult.NextPaginationToken)
+		assert.Len(t, paginationResult.Users, 1)
+		assert.Equal(t, paginationResult.Users[0].Emails[0], "test3@gmail.com")
+
+		limit = 5
+		paginationResult, err = supertokens.GetUsersNewestFirst("public", paginationResult.NextPaginationToken, &limit, nil, nil)
+		if err != nil {
+			t.Error(err)
+			return
+		}
+
+		assert.Nil(t, paginationResult.NextPaginationToken)
+		assert.Len(t, paginationResult.Users, 3)
+		assert.Equal(t, paginationResult.Users[0].Emails[0], "test2@gmail.com")
+	}
+
+	{
+		paginationToken := "invalid"
+		_, err := supertokens.GetUsersNewestFirst("public", &paginationToken, nil, nil, nil)
+		if err != nil {
+			assert.Contains(t, err.Error(), "invalid pagination token")
+		} else {
+			assert.Fail(t, "pagination token invalid should fail")
+		}
+	}
+}
+
+func TestGetOldestUsersFirstWithSearchParams(t *testing.T) {
+	BeforeEach()
+	unittesting.StartUpST("localhost", "8080")
+	defer AfterEach()
+	telemetry := false
+	supertokens.Init(supertokens.TypeInput{
+		Supertokens: &supertokens.ConnectionInfo{
+			ConnectionURI: "http://localhost:8080",
+		},
+		AppInfo: supertokens.AppInfo{
+			AppName:   "Testing",
+			Origin:    "http://localhost:3000",
+			APIDomain: "http://localhost:3001",
+		},
+		Telemetry: &telemetry,
+		RecipeList: []supertokens.Recipe{
+			Init(nil),
+		},
+	})
+
+	_, err := SignUp("public", "test@gmail.com", "testPass123")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	_, err = SignUp("public", "test1@gmail.com", "testPass123")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	_, err = SignUp("public", "test2@gmail.com", "testPass123")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	_, err = SignUp("public", "test3@gmail.com", "testPass123")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	_, err = SignUp("public", "john@gmail.com", "testPass123")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	{
+		paginationResult, err := supertokens.GetUsersOldestFirst("public", nil, nil, nil, map[string]string{
+			"email": "doe",
+		})
+		if err != nil {
+			t.Error(err)
+			return
+		}
+
+		assert.Nil(t, paginationResult.NextPaginationToken)
+		assert.Len(t, paginationResult.Users, 0)
+	}
+
+	{
+		paginationResult, err := supertokens.GetUsersOldestFirst("public", nil, nil, nil, map[string]string{
+			"email": "john",
+		})
+		if err != nil {
+			t.Error(err)
+			return
+		}
+
+		assert.Nil(t, paginationResult.NextPaginationToken)
+		assert.Len(t, paginationResult.Users, 1)
+		assert.Equal(t, paginationResult.Users[0].Emails[0], "john@gmail.com")
+		assert.Len(t, paginationResult.Users[0].LoginMethods, 1)
+		assert.Len(t, paginationResult.Users[0].Emails, 1)
+		assert.Len(t, paginationResult.Users[0].PhoneNumbers, 0)
+		assert.Len(t, paginationResult.Users[0].ThirdParty, 0)
+	}
+}
+
+func TestGetNewestUsersFirstWithSearchParams(t *testing.T) {
+	BeforeEach()
+	unittesting.StartUpST("localhost", "8080")
+	defer AfterEach()
+	telemetry := false
+	supertokens.Init(supertokens.TypeInput{
+		Supertokens: &supertokens.ConnectionInfo{
+			ConnectionURI: "http://localhost:8080",
+		},
+		AppInfo: supertokens.AppInfo{
+			AppName:   "Testing",
+			Origin:    "http://localhost:3000",
+			APIDomain: "http://localhost:3001",
+		},
+		Telemetry: &telemetry,
+		RecipeList: []supertokens.Recipe{
+			Init(nil),
+		},
+	})
+
+	_, err := SignUp("public", "test@gmail.com", "testPass123")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	_, err = SignUp("public", "test1@gmail.com", "testPass123")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	_, err = SignUp("public", "test2@gmail.com", "testPass123")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	_, err = SignUp("public", "test3@gmail.com", "testPass123")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	_, err = SignUp("public", "john@gmail.com", "testPass123")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	{
+		paginationResult, err := supertokens.GetUsersNewestFirst("public", nil, nil, nil, map[string]string{
+			"email": "doe",
+		})
+		if err != nil {
+			t.Error(err)
+			return
+		}
+
+		assert.Nil(t, paginationResult.NextPaginationToken)
+		assert.Len(t, paginationResult.Users, 0)
+	}
+
+	{
+		paginationResult, err := supertokens.GetUsersNewestFirst("public", nil, nil, nil, map[string]string{
+			"email": "john",
+		})
+		if err != nil {
+			t.Error(err)
+			return
+		}
+
+		assert.Nil(t, paginationResult.NextPaginationToken)
+		assert.Len(t, paginationResult.Users, 1)
+		assert.Equal(t, paginationResult.Users[0].Emails[0], "john@gmail.com")
+		assert.Len(t, paginationResult.Users[0].LoginMethods, 1)
+		assert.Len(t, paginationResult.Users[0].Emails, 1)
+		assert.Len(t, paginationResult.Users[0].PhoneNumbers, 0)
+		assert.Len(t, paginationResult.Users[0].ThirdParty, 0)
+	}
+}
