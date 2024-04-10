325bfae810f25863ae75b27c384c3ab8e1649143
rishabhpoddar <rishabh.poddar@gmail.com>
2024-04-10 14:36:30 +0530

    more test transfers

--- recipe/thirdparty/provider_test.go
+++ recipe/thirdparty/provider_test.go
@@ -19,7 +19,6 @@ package thirdparty
 import (
 	"encoding/json"
 	"errors"
-	"github.com/supertokens/supertokens-golang/recipe/session"
 	"io"
 	"io/ioutil"
 	"net"
@@ -29,12 +28,417 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/supertokens/supertokens-golang/recipe/session"
+	"github.com/supertokens/supertokens-golang/recipe/session/sessmodels"
+
 	"github.com/stretchr/testify/assert"
 	"github.com/supertokens/supertokens-golang/recipe/thirdparty/tpmodels"
 	"github.com/supertokens/supertokens-golang/supertokens"
 	"github.com/supertokens/supertokens-golang/test/unittesting"
 	"gopkg.in/h2non/gock.v1"
 )
+
+const privateKey = "-----BEGIN PRIVATE KEY-----\nMIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQgu8gXs+XYkqXD6Ala9Sf/iJXzhbwcoG5dMh1OonpdJUmgCgYIKoZIzj0DAQehRANCAASfrvlFbFCYqn3I2zeknYXLwtH30JuOKestDbSfZYxZNMqhF/OzdZFTV0zc5u5s3eN+oCWbnvl0hM+9IW0UlkdA\n-----END PRIVATE KEY-----"
+
+func TestWithAdditionalParamsCheckTheyArePresentInAuthorizationURLForThirdPartyProviderApple(t *testing.T) {
+	clientId := "test"
+
+	configValue := supertokens.TypeInput{
+		Supertokens: &supertokens.ConnectionInfo{
+			ConnectionURI: "http://localhost:8080",
+		},
+		AppInfo: supertokens.AppInfo{
+			APIDomain:     "api.supertokens.io",
+			AppName:       "SuperTokens",
+			WebsiteDomain: "supertokens.io",
+		},
+		RecipeList: []supertokens.Recipe{
+			session.Init(&sessmodels.TypeInput{
+				GetTokenTransferMethod: func(req *http.Request, forCreateNewSession bool, userContext supertokens.UserContext) sessmodels.TokenTransferMethod {
+					return sessmodels.CookieTransferMethod
+				},
+			}),
+			Init(&tpmodels.TypeInput{
+				SignInAndUpFeature: tpmodels.TypeInputSignInAndUp{
+					Providers: []tpmodels.ProviderInput{
+						{
+							Config: tpmodels.ProviderConfig{
+								ThirdPartyId: "apple",
+								Clients: []tpmodels.ProviderClientConfig{
+									{
+										ClientID: clientId,
+										AdditionalConfig: map[string]interface{}{
+											"keyId":      "test-key",
+											"privateKey": privateKey,
+											"teamId":     "test-team-id",
+										},
+									},
+								},
+								AuthorizationEndpointQueryParams: map[string]interface{}{
+									"key1": "value1",
+									"key2": "value2",
+								},
+							},
+						},
+					},
+				},
+			}),
+		},
+	}
+
+	BeforeEach()
+	unittesting.StartUpST("localhost", "8080")
+	defer AfterEach()
+	err := supertokens.Init(configValue)
+	if err != nil {
+		t.Error(err.Error())
+	}
+	q, err := supertokens.GetNewQuerierInstanceOrThrowError("")
+	if err != nil {
+		t.Error(err.Error())
+	}
+	apiV, err := q.GetQuerierAPIVersion()
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	if unittesting.MaxVersion(apiV, "2.11") == "2.11" {
+		return
+	}
+
+	providerRes, err := GetProvider("public", "apple", nil)
+	assert.NoError(t, err)
+
+	providerInfo := providerRes
+
+	assert.Equal(t, "apple", providerInfo.ID)
+
+	assert.Equal(t, "https://appleid.apple.com/auth/authorize", providerInfo.Config.AuthorizationEndpoint)
+	assert.Equal(t, "https://appleid.apple.com/auth/token", providerInfo.Config.TokenEndpoint)
+
+	authUrlRes, err := providerInfo.GetAuthorisationRedirectURL("redirect", &map[string]interface{}{})
+	assert.NoError(t, err)
+
+	urlObj, err := url.Parse(authUrlRes.URLWithQueryParams)
+	assert.NoError(t, err)
+
+	authParams := urlObj.Query()
+
+	assert.Equal(t, url.Values{
+		"client_id":     {"test"},
+		"response_mode": {"form_post"},
+		"response_type": {"code"},
+		"redirect_uri":  {"redirect"},
+		"scope":         {"openid email"},
+		"key1":          {"value1"},
+		"key2":          {"value2"},
+	}, authParams)
+}
+
+func TestWithPassingScopesInConfigForThirdPartyProviderApple(t *testing.T) {
+	clientId := "test"
+
+	configValue := supertokens.TypeInput{
+		Supertokens: &supertokens.ConnectionInfo{
+			ConnectionURI: "http://localhost:8080",
+		},
+		AppInfo: supertokens.AppInfo{
+			APIDomain:     "api.supertokens.io",
+			AppName:       "SuperTokens",
+			WebsiteDomain: "supertokens.io",
+		},
+		RecipeList: []supertokens.Recipe{
+			session.Init(&sessmodels.TypeInput{
+				GetTokenTransferMethod: func(req *http.Request, forCreateNewSession bool, userContext supertokens.UserContext) sessmodels.TokenTransferMethod {
+					return sessmodels.CookieTransferMethod
+				},
+			}),
+			Init(&tpmodels.TypeInput{
+				SignInAndUpFeature: tpmodels.TypeInputSignInAndUp{
+					Providers: []tpmodels.ProviderInput{
+						{
+							Config: tpmodels.ProviderConfig{
+								ThirdPartyId: "apple",
+								Clients: []tpmodels.ProviderClientConfig{
+									{
+										ClientID: clientId,
+										Scope:    []string{"test-scope-1", "test-scope-2"},
+										AdditionalConfig: map[string]interface{}{
+											"keyId":      "test-key",
+											"privateKey": privateKey,
+											"teamId":     "test-team-id",
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			}),
+		},
+	}
+
+	BeforeEach()
+	unittesting.StartUpST("localhost", "8080")
+	defer AfterEach()
+	err := supertokens.Init(configValue)
+	if err != nil {
+		t.Error(err.Error())
+	}
+	q, err := supertokens.GetNewQuerierInstanceOrThrowError("")
+	if err != nil {
+		t.Error(err.Error())
+	}
+	apiV, err := q.GetQuerierAPIVersion()
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	if unittesting.MaxVersion(apiV, "2.11") == "2.11" {
+		return
+	}
+
+	providerRes, err := GetProvider("public", "apple", nil)
+	assert.NoError(t, err)
+
+	providerInfo := providerRes
+
+	assert.Equal(t, "apple", providerInfo.ID)
+
+	assert.Equal(t, "https://appleid.apple.com/auth/authorize", providerInfo.Config.AuthorizationEndpoint)
+	assert.Equal(t, "https://appleid.apple.com/auth/token", providerInfo.Config.TokenEndpoint)
+
+	authUrlRes, err := providerInfo.GetAuthorisationRedirectURL("redirect", &map[string]interface{}{})
+	assert.NoError(t, err)
+
+	urlObj, err := url.Parse(authUrlRes.URLWithQueryParams)
+	assert.NoError(t, err)
+
+	authParams := urlObj.Query()
+
+	assert.Equal(t, url.Values{
+		"client_id":     {"test"},
+		"response_mode": {"form_post"},
+		"response_type": {"code"},
+		"redirect_uri":  {"redirect"},
+		"scope":         {"test-scope-1 test-scope-2"},
+	}, authParams)
+}
+
+func TestWithMinimumConfigForThirdPartyProviderApple(t *testing.T) {
+	clientId := "test"
+
+	configValue := supertokens.TypeInput{
+		Supertokens: &supertokens.ConnectionInfo{
+			ConnectionURI: "http://localhost:8080",
+		},
+		AppInfo: supertokens.AppInfo{
+			APIDomain:     "api.supertokens.io",
+			AppName:       "SuperTokens",
+			WebsiteDomain: "supertokens.io",
+		},
+		RecipeList: []supertokens.Recipe{
+			session.Init(&sessmodels.TypeInput{
+				GetTokenTransferMethod: func(req *http.Request, forCreateNewSession bool, userContext supertokens.UserContext) sessmodels.TokenTransferMethod {
+					return sessmodels.CookieTransferMethod
+				},
+			}),
+			Init(&tpmodels.TypeInput{
+				SignInAndUpFeature: tpmodels.TypeInputSignInAndUp{
+					Providers: []tpmodels.ProviderInput{
+						{
+							Config: tpmodels.ProviderConfig{
+								ThirdPartyId: "apple",
+								Clients: []tpmodels.ProviderClientConfig{
+									{
+										ClientID: clientId,
+										AdditionalConfig: map[string]interface{}{
+											"keyId":      "test-key",
+											"privateKey": privateKey,
+											"teamId":     "test-team-id",
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			}),
+		},
+	}
+
+	BeforeEach()
+	unittesting.StartUpST("localhost", "8080")
+	defer AfterEach()
+	err := supertokens.Init(configValue)
+	if err != nil {
+		t.Error(err.Error())
+	}
+	q, err := supertokens.GetNewQuerierInstanceOrThrowError("")
+	if err != nil {
+		t.Error(err.Error())
+	}
+	apiV, err := q.GetQuerierAPIVersion()
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	if unittesting.MaxVersion(apiV, "2.11") == "2.11" {
+		return
+	}
+
+	providerRes, err := GetProvider("public", "apple", nil)
+	assert.NoError(t, err)
+
+	providerInfo := providerRes
+
+	assert.Equal(t, "apple", providerInfo.ID)
+
+	assert.Equal(t, "https://appleid.apple.com/auth/authorize", providerInfo.Config.AuthorizationEndpoint)
+	assert.Equal(t, "https://appleid.apple.com/auth/token", providerInfo.Config.TokenEndpoint)
+
+	authUrlRes, err := providerInfo.GetAuthorisationRedirectURL("redirect", &map[string]interface{}{})
+	assert.NoError(t, err)
+
+	urlObj, err := url.Parse(authUrlRes.URLWithQueryParams)
+	assert.NoError(t, err)
+
+	authParams := urlObj.Query()
+
+	assert.Equal(t, url.Values{
+		"client_id":     {"test"},
+		"response_mode": {"form_post"},
+		"response_type": {"code"},
+		"redirect_uri":  {"redirect"},
+		"scope":         {"openid email"},
+	}, authParams)
+
+	tokenParams := url.Values{}
+
+	defer gock.OffAll()
+	gock.New("https://appleid.apple.com").
+		Post("/auth/token").
+		Persist().
+		Map(func(r *http.Request) *http.Request {
+			data, err := ioutil.ReadAll(r.Body)
+			assert.NoError(t, err)
+			tokenParams, err = url.ParseQuery(string(data))
+			assert.NoError(t, err)
+			return r
+		}).
+		Reply(200).
+		JSON(map[string]string{
+			"id_token": "abcd",
+		})
+
+	_, err = providerInfo.ExchangeAuthCodeForOAuthTokens(tpmodels.TypeRedirectURIInfo{
+		RedirectURIOnProviderDashboard: "redirect",
+		RedirectURIQueryParams: map[string]interface{}{
+			"code": "abcd",
+		},
+	}, &map[string]interface{}{})
+	assert.NoError(t, err)
+
+	assert.NotEmpty(t, tokenParams.Get("client_secret"))
+	tokenParams.Del("client_secret")
+
+	assert.Equal(t, url.Values{
+		"client_id":    {"test"},
+		"grant_type":   {"authorization_code"},
+		"code":         {"abcd"},
+		"redirect_uri": {"redirect"},
+	}, tokenParams)
+}
+
+func TestPassingAdditionalParamsCheckTheyArePresentInAuthorizationUrlForThirdPartyProviderGoogle(t *testing.T) {
+	configValue := supertokens.TypeInput{
+		Supertokens: &supertokens.ConnectionInfo{
+			ConnectionURI: "http://localhost:8080",
+		},
+		AppInfo: supertokens.AppInfo{
+			APIDomain:     "api.supertokens.io",
+			AppName:       "SuperTokens",
+			WebsiteDomain: "supertokens.io",
+		},
+		RecipeList: []supertokens.Recipe{
+			session.Init(&sessmodels.TypeInput{
+				GetTokenTransferMethod: func(req *http.Request, forCreateNewSession bool, userContext supertokens.UserContext) sessmodels.TokenTransferMethod {
+					return sessmodels.CookieTransferMethod
+				},
+			}),
+			Init(&tpmodels.TypeInput{
+				SignInAndUpFeature: tpmodels.TypeInputSignInAndUp{
+					Providers: []tpmodels.ProviderInput{
+						{
+							Config: tpmodels.ProviderConfig{
+								ThirdPartyId: "google",
+								Clients: []tpmodels.ProviderClientConfig{
+									{
+										ClientID:     "test",
+										ClientSecret: "test-secret",
+									},
+								},
+								AuthorizationEndpointQueryParams: map[string]interface{}{
+									"key1": "value1",
+									"key2": "value2",
+								},
+							},
+						},
+					},
+				},
+			}),
+		},
+	}
+
+	BeforeEach()
+	unittesting.StartUpST("localhost", "8080")
+	defer AfterEach()
+	err := supertokens.Init(configValue)
+	if err != nil {
+		t.Error(err.Error())
+	}
+	q, err := supertokens.GetNewQuerierInstanceOrThrowError("")
+	if err != nil {
+		t.Error(err.Error())
+	}
+	apiV, err := q.GetQuerierAPIVersion()
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	if unittesting.MaxVersion(apiV, "2.11") == "2.11" {
+		return
+	}
+
+	providerRes, err := GetProvider("public", "google", nil)
+	assert.NoError(t, err)
+
+	providerInfo := providerRes
+	assert.Equal(t, "google", providerInfo.ID)
+
+	assert.Equal(t, "https://oauth2.googleapis.com/token", providerInfo.Config.TokenEndpoint)
+	assert.Equal(t, "https://accounts.google.com/o/oauth2/v2/auth", providerInfo.Config.AuthorizationEndpoint)
+	assert.Equal(t, "https://openidconnect.googleapis.com/v1/userinfo", providerInfo.Config.UserInfoEndpoint)
+
+	authUrlRes, err := providerInfo.GetAuthorisationRedirectURL("redirect", &map[string]interface{}{})
+	assert.NoError(t, err)
+
+	urlObj, err := url.Parse(authUrlRes.URLWithQueryParams)
+	assert.NoError(t, err)
+
+	authParams := urlObj.Query()
+
+	assert.Equal(t, url.Values{
+		"client_id":              {"test"},
+		"access_type":            {"offline"},
+		"include_granted_scopes": {"true"},
+		"response_type":          {"code"},
+		"redirect_uri":           {"redirect"},
+		"scope":                  {"openid email"},
+		"key1":                   {"value1"},
+		"key2":                   {"value2"},
+	}, authParams)
+}
 
 func TestMinimumConfigForGoogleAsThirdPartyProvider(t *testing.T) {
 	configValue := supertokens.TypeInput{
@@ -900,4 +1304,60 @@ func TestThatSignInUpWorksIfValidateAccessTokenDoesNotReturnError(t *testing.T) 
 	assert.Equal(t, res.StatusCode, 200)
 	assert.True(t, overrideValidateCalled)
 	assert.Equal(t, response["status"], "OK")
+}
+
+func TestWithDuplicateProvider(t *testing.T) {
+	configValue := supertokens.TypeInput{
+		Supertokens: &supertokens.ConnectionInfo{
+			ConnectionURI: "http://localhost:8080",
+		},
+		AppInfo: supertokens.AppInfo{
+			APIDomain:     "api.supertokens.io",
+			AppName:       "SuperTokens",
+			WebsiteDomain: "supertokens.io",
+		},
+		RecipeList: []supertokens.Recipe{
+			session.Init(&sessmodels.TypeInput{
+				GetTokenTransferMethod: func(req *http.Request, forCreateNewSession bool, userContext supertokens.UserContext) sessmodels.TokenTransferMethod {
+					return sessmodels.CookieTransferMethod
+				},
+			}),
+			Init(&tpmodels.TypeInput{
+				SignInAndUpFeature: tpmodels.TypeInputSignInAndUp{
+					Providers: []tpmodels.ProviderInput{
+						{
+							Config: tpmodels.ProviderConfig{
+								ThirdPartyId: "google",
+								Clients: []tpmodels.ProviderClientConfig{
+									{
+										ClientID:     "test",
+										ClientSecret: "test-secret",
+									},
+								},
+							},
+						},
+						{
+							Config: tpmodels.ProviderConfig{
+								ThirdPartyId: "google",
+								Clients: []tpmodels.ProviderClientConfig{
+									{
+										ClientID:     "test",
+										ClientSecret: "test-secret",
+									},
+								},
+							},
+						},
+					},
+				},
+			}),
+		},
+	}
+
+	BeforeEach()
+	unittesting.StartUpST("localhost", "8080")
+	defer AfterEach()
+	err := supertokens.Init(configValue)
+	if err != nil {
+		assert.Equal(t, "The providers array has multiple entries for the same third party provider.", err.Error())
+	}
 }
